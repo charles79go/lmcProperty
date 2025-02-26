1d638552743ae561c16a7544d434c738770087dd
amos <amos@saltboxmgmt.com>
2025-02-25 22:03:55 -0500

    card and image init

---
+++ force-app/main/default/lwc/propertyCard/propertyCard.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import propCabinImage from '@salesforce/resourceUrl/propCabinImage';
+
+export default class PropertyCard extends LightningElement {
+    propCabinImage = propCabinImage;
+}
